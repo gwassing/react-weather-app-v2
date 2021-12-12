69b017aaef0243531baa9428459a257a587daed3
gabyypants <86713756+gabyypants@users.noreply.github.com>
2021-12-12 20:37:32 +0100

    Removed React.StrictMode to prevent double (triple) rendering

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
