9d882cd852ab063d3698833fbcd515cbfb561e5c
gabyypants <86713756+gabyypants@users.noreply.github.com>
2021-12-13 17:51:59 +0100

    updated formatted date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function FormattedDate(props) {
   console.log(props.date);
-
   let days = [
     "Sunday",
     "Monday",
@@ -12,19 +11,49 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   let day = days[props.date.getDay()];
-  let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+  let date = props.date.getDate();
+  let month = months[props.date.getMonth()];
 
-  return (
-    <div>
-      {day} {hours}:{minutes}
-    </div>
-  );
+  if (date === 1 || date === 21 || date === 31) {
+    return (
+      <div>
+        {day}, {date}st of {month}
+      </div>
+    );
+  }
+  if (date === 2 || date === 22) {
+    return (
+      <div>
+        {day}, {date}nd of {month}
+      </div>
+    );
+  }
+  if (date === 3 || date === 23) {
+    return (
+      <div>
+        {day}, {date}rd of {month}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {day}, {date}th of {month}
+      </div>
+    );
+  }
 }
